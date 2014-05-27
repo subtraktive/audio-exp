dd9669cdc0e0ae86158b2ecda1ede2f177cb3619
Arun kumar <arunkumar.nie@gmail.com>
2014-05-27 19:24:37 +0530

    loopable feature added

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,22 +1,74 @@
 'use strict';
 
-audioExp.controller('MainCtrl', ['$scope', 'synth', 'bufferLoader', function ($scope, Synth, BufferLoader) {
+audioExp.controller('MainCtrl', ['$scope', 'synth', 'bufferLoader', 'audio', function ($scope, Synth, BufferLoader, audio) {
 
-	var context = new webkitAudioContext();
-  	var synth1 = $scope.synth = new Synth(context);
-	//synth = new Synth(audiocontext);
+	//var context = new webkitAudioContext();
+  
+  //var synth1 = $scope.synth = new Synth(context);
   	
-  	$scope.synth.oscillator.type = 2;
+  //$scope.synth.oscillator.type = 2;
 
-    var bufferLoader = new BufferLoader(
-        context,
-        [
-        "../sound/kick.wav"
-        ],
-        finishedLoading
-    );
-
-    bufferLoader.load();
+   try {
+        // More info at http://caniuse.com/#feat=audio-api
+        window.AudioContext = window.AudioContext || window.webkitAudioContext;
+        audio.context = new webkitAudioContext();
+        var synth1 = $scope.synth = new Synth(audio.context);
+    } catch(e) {
+        audio.proceed = false;
+        alert('Web Audio API not supported in this browser.');
+    }
+ 
+    if (audio.proceed) {
+        //---------------
+        // Compatibility
+        //---------------
+        (function() {
+            var start = 'start',
+                stop = 'stop',
+                buffer = audio.context.createBufferSource();
+ 
+            if (typeof buffer.start !== 'function') {
+                start = 'noteOn';
+            }
+            audio.compatibility.start = start;
+ 
+            if (typeof buffer.stop !== 'function') {
+                stop = 'noteOff';
+            }
+            audio.compatibility.stop = stop;
+        })();
+ 
+        //-------------------------------
+        // Setup Audio Files and Buttons
+        //-------------------------------
+        for (var a in audio.files) {
+            (function() {
+                var i = parseInt(a) + 1;
+                var req = new XMLHttpRequest();
+                req.open('GET', audio.files[i - 1], true); // array starts with 0 hence the -1
+                req.responseType = 'arraybuffer';
+                req.onload = function() {
+                    audio.context.decodeAudioData(
+                        req.response,
+                        function(buffer) {
+                            audio.buffer[i] = buffer;
+                            audio.source_loop[i] = {};
+                            var button = document.getElementById('button-loop-' + i);
+                            button.addEventListener('click', function(e) {
+                                e.preventDefault();
+                                audio.play(this.value);
+                            });
+                        },
+                        function() {
+                            console.log('Error decoding audio "' + audio.files[i - 1] + '".');
+                        }
+                    );
+                };
+                req.send();
+            })();
+        }
+    }
+ 
 
   	$scope.playNote = function(note){
   		synth1.noteOn(note)
@@ -41,18 +93,7 @@ audioExp.controller('MainCtrl', ['$scope', 'synth', 'bufferLoader', function ($s
 		}, 300)
 	}
 
-  function finishedLoading(bufferList) {
-    // Create two sources and play them both together.
-    var source1 = context.createBufferSource();
-    var source2 = context.createBufferSource();
-    source1.buffer = bufferList[0];
-    source2.buffer = bufferList[1];
-    
-    source1.connect(context.destination);
-    source2.connect(context.destination);
-    source1.noteOn(0);
-    source2.noteOn(0);
-}
+  
 
 
 
